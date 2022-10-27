740175351de1eeed917889aa6ae42b94d8b8c994
James Earle <j_earle@hotmail.com>
2022-10-27 15:01:56 -0700

    a small change that was needed for shift to goerli

--- src/hub.ts
+++ src/hub.ts
@@ -61,10 +61,10 @@ export function handleDomainCreatedV2(event: EEDomainCreatedV2): void {
     setupGlobalTracker(domain);
   }
 
-  if (domainParent.name == null) {
+  if (!domainParent.name) {
     domain.name = event.params.label;
   } else {
-    domain.name = domainParent.name.concat(".").concat(event.params.label);
+    domain.name = domainParent.name!.concat(".").concat(event.params.label);
   }
   domain.label = event.params.label;
   domain.labelHash = event.params.labelHash.toHex();
@@ -120,10 +120,10 @@ export function handleDomainCreatedV3(event: EEDomainCreatedV3): void {
     setupGlobalTracker(domain);
   }
 
-  if (domainParent.name == null) {
+  if (!domainParent.name) {
     domain.name = event.params.label;
   } else {
-    domain.name = domainParent.name.concat(".").concat(event.params.label);
+    domain.name = domainParent.name!.concat(".").concat(event.params.label);
   }
   domain.label = event.params.label;
   domain.labelHash = event.params.labelHash.toHex();
